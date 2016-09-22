2ed334945926221835f3b946ab1b192598edb7e8
Daniel Blanco <daniel@blancomartin.com.ar>
2016-09-22 08:59:30 -0300

    Create updatewsfev1.sh

---
+++ updatewsfev1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Este archivo sirve para actualizar un solo archivo correspondiente a los ws afip
+cd /opt/odoo
+curl -O https://raw.githubusercontent.com/reingart/pyafipws/master/wsfev1.py
